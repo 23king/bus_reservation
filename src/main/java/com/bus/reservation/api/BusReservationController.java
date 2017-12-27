c5829bdf11e5fd49fd0edf55f17ff4e7cca1ee93
이세왕(Sewang Lee)/Platform Architecture팀/SKP <swlee0921@sk.com>
2017-12-27 15:03:01 +0900

    jsp setting

--- src/main/java/com/bus/reservation/api/BusReservationController.java
+++ src/main/java/com/bus/reservation/api/BusReservationController.java
@@ -1,18 +1,17 @@
 package com.bus.reservation.api;
 
 import com.bus.reservation.domain.model.BusReservation;
-import com.bus.reservation.domain.model.BusTemplate;
 import com.bus.reservation.domain.model.User;
 import com.bus.reservation.domain.repository.BusReservationRepository;
 import com.bus.reservation.domain.repository.BusTemplateRepository;
 import com.bus.reservation.domain.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "/client")
 public class BusReservationController {
     @Autowired
@@ -22,7 +21,7 @@ public class BusReservationController {
     @Autowired
     private MemberRepository memberRepository;
 
-    @GetMapping(value="/findUser")
+    @RequestMapping(value="/findUser")
     public User findUser(@RequestParam String user_name,@RequestParam String user_phone){
         return memberRepository.findByUserNameAndUserPhone(user_name, user_phone);
     }
